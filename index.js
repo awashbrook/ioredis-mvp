177dc8132f71a07d2142465229fa3ab239d487ad
awashbrook <andy.washbrook@thomsonreuters.com>
2021-03-19 20:34:11 +0000

    Password on Cmdline

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const Redis = require("ioredis");
 
+const qualificationKey = "qualification:node:test:1";
+const maxNumRange = 100;
+
+var params = process.argv.slice(2);
 var redisParams = {
     host: 'localhost',
     port: '6379'
   };
-  redisParams.password = "CAROLINA-regiment-mere-shot";
-  // console.log(redisParams);
+  if (params[0]) {
+    redisParams.password = params[0];
+  } 
+//   console.log(redisParams);
   
-const redis = new Redis(redisParams);
+const client = new Redis(redisParams); 
 
-redis.set("mykey", "Hello from io-redis!");
-redis.get("mykey", function(err, result){
-    if (err) { throw err; }
-
-    console.log(result);
-});
+client.on("error", function(error) {
+    console.error(error);
+  });
+  
+client.unlink(qualificationKey);
+// TODO Pipeline for more efficiency
+// for(var i = 0; i < maxNumRange ;i++) {
+//     client.lpush(qualificationKey, i, client.print);
+// }
+client.set(qualificationKey, "Hello from Ember!");
 
 // use the promise instead of callback
-redis.get("mykey").then(function (result) {
-    console.log(result);
-});
+client.get(qualificationKey).then((result) => console.log(result));
 
-redis.zadd("vehicles", 0, "car", 0, "bike");
-redis.zrange("vehicles", 0, -1, "WITHSCORES").then((res) => console.log(res));
+client.zadd("vehicles", 0, "car", 0, "bike");
+client.zrange("vehicles", 0, -1, "WITHSCORES").then((res) => console.log(res));
+// TODO ZREVRANGE
