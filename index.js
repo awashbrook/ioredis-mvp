54a37bb76aca9bc85201634ac1f789278ad7f874
awashbrook <andy.washbrook@thomsonreuters.com>
2021-03-20 19:24:35 +0000

    Working prior to refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Redis = require("ioredis");
 
 const qualificationKey = "qualification:node:test:1";
-const maxNumRange = 100;
+const maxNumRange = 2;
 
 var params = process.argv.slice(2);
 var redisParams = {
@@ -11,24 +11,33 @@ var redisParams = {
   if (params[0]) {
     redisParams.password = params[0];
   } 
-//   console.log(redisParams);
   
 const client = new Redis(redisParams); 
 
 client.on("error", function(error) {
     console.error(error);
   });
-  
+
 client.unlink(qualificationKey);
+
 // TODO Pipeline for more efficiency
-// for(var i = 0; i < maxNumRange ;i++) {
-//     client.lpush(qualificationKey, i, client.print);
+
+for(var i = 0; i < maxNumRange ;i++) {
+    client.lpush(qualificationKey, i); // Doesn't print with IORedis
+}
+
+// TODO Split these up
+
+// Fails without promises!
+// for(var i = 100; i > 0 ;i--) {
+//     client.lpop(qualificationKey).then((res) => console.log(res));
 // }
-client.set(qualificationKey, "Hello from Ember!");
 
-// use the promise instead of callback
-client.get(qualificationKey).then((result) => console.log(result));
+// client.set(qualificationKey, "Hello!");
+// // use the promise instead of callback
+// client.get(qualificationKey).then((result) => console.log(result));
 
+// TODO ZREVRANGE
 client.zadd("vehicles", 0, "car", 0, "bike");
 client.zrange("vehicles", 0, -1, "WITHSCORES").then((res) => console.log(res));
-// TODO ZREVRANGE
+
