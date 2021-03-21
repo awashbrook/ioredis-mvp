02b61bef38a8168144041f12ee1485a9937f2d96
awashbrook <awashbrook@gmail.com>
2021-03-21 14:17:35 +0000

    Supporting npm and yarn

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   qualificationKey: "qualification:node:test:1",
-  maxNumRange: 2,
+  maxNumRange: 100,
   redisParams: {
     host: 'localhost',
     port: '6379'  
@@ -8,7 +8,9 @@ const config = {
   // REDIS_URL: process.env.BIGFOOT_DATA_SERVICE_REDIS_URL ?? 'redis://localhost:6379/0'
 };
 
-var params = process.argv.slice(2);
+console.log('Using Redis Params:');
+console.log(config.redisParams);
+let params = process.argv.slice(2);
 if (params[0]) {
   config.redisParams.password = params[0];
 } 
