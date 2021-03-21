e052f6b9e124e35ddb2978739757c1fc04f69939
awashbrook <andy.washbrook@thomsonreuters.com>
2021-03-21 17:39:18 +0000

    Add load start/end times

--- load-list.js
+++ load-list.js
@@ -11,8 +11,12 @@ client.unlink(config.qualificationKey);
 
 // TODO Pipeline for more efficiency
 
+console.log('Starting data load at:' + new Date());
+ 
 for(var i = 0; i < config.maxNumRange ;i++) {
-    client.lpush(config.qualificationKey, i); 
+   client.lpush(config.qualificationKey, i);
 }
+ 
+console.log('Ending data load at:' + new Date());
 
 client.quit();
--- load-sorted-set.js
+++ load-sorted-set.js
@@ -10,8 +10,14 @@ client.on("error", function(error) {
 
 client.unlink(config.qualificationKey);
 
+console.log('Starting data load at:' + new Date());
+
 // TODO ZREVRANGE
 client.zadd(config.qualificationKey, 0, "car", 0, "bike");
+
+// TODO Separate Retrieve
 client.zrange(config.qualificationKey, 0, -1, "WITHSCORES").then((res) => console.log(res));
+
+console.log('Ending data load at:' + new Date());
 
 client.quit();
